7396e4e7d6156a1c7bfdac3d2df1dbe4ca01b778
Ravindu Samaraweera <155237294+rasandu17@users.noreply.github.com>
2025-05-31 21:44:22 +0530

    Form validations

--- add.php
+++ add.php
@@ -1,14 +1,27 @@
 <?php
 
 if (isset($_POST['submit'])) {
-    echo htmlspecialchars($_POST['email']);
-    echo htmlspecialchars($_POST['title']);
-    echo htmlspecialchars($_POST['ingredients']);
+    
+    if(empty($_POST['email'])){
+        echo'An email is required <br/>'; 
+    }else{
+        echo htmlspecialchars($_POST['email']);
+    }
+
+    if(empty($_POST['title'])){
+        echo'A title is required <br/>'; 
+    }else{
+        echo htmlspecialchars($_POST['title']);
+    }
+
+    if(empty($_POST['ingredients'])){
+        echo'At least one ingredient is required <br/>'; 
+    }else{
+        echo htmlspecialchars($_POST['ingredients']);
+    }
 }
 
 ?>
-
-
 
 
 <!DOCTYPE html>
