26a17523c3b468f92fe31ee888cde08762572386
Ravindu Samaraweera <155237294+rasandu17@users.noreply.github.com>
2025-05-31 15:48:42 +0530

    Variables and Constants

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+    define('NAME' , 'Rukiee');
+    //$name = "Rukiee";
+    //echo $name;
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My php</title>
+</head>
+<body>
+    <h1>User profile</h1>
+    <div><?php echo NAME;?></div>
+</body>
+</html>
