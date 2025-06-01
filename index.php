574f32e9c01b59ca4a0e893cff0266205f014e86
Ravindu Samaraweera <155237294+rasandu17@users.noreply.github.com>
2025-06-01 18:26:58 +0530

    Redesign pizza display to improve ingredient listing

--- index.php
+++ index.php
@@ -13,6 +13,8 @@ $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 mysqli_free_result($result);
 mysqli_close($conn);
 
+//explode(',', $pizzas[0]['ingredients']);
+
 ?>
 
 
@@ -25,20 +27,26 @@ mysqli_close($conn);
 <h4 class="center">Pizzas!!!</h4>
 <div class="container">
     <div class="row">
-        <?php foreach($pizzas as $pizza){?>
+        <?php foreach ($pizzas as $pizza) { ?>
 
             <div class="col s6 md3">
                 <div class="card z-depth-0">
                     <div class="card-content center">
-                        <h6><?php echo htmlspecialchars($pizza['title'])?></h6>
-                        <div><?php echo htmlspecialchars($pizza['ingredients'])?></div>
+                        <h6><?php echo htmlspecialchars($pizza['title']) ?></h6>
+                        <div>
+                            <ul>
+                                <?php foreach (explode(',', $pizza['ingredients']) as $ing) { ?>
+                                    <li><?php echo htmlspecialchars($ing) ?></li>
+                                <?php } ?>
+                            </ul>
+                        </div>
                     </div>
                     <div class="card-action right-align">
                         <a class="brand-text" href="#">more info</a>
                     </div>
                 </div>
             </div>
-            <?php } ?>
+        <?php } ?>
     </div>
 </div>
 
