07fcd42a03a245d22cdb16d27d4c7234fb8a624a
Ravindu Samaraweera <155237294+rasandu17@users.noreply.github.com>
2025-06-01 14:40:04 +0530

    Add form error handling to use an errors array for better validation feedback

--- add.php
+++ add.php
@@ -1,31 +1,34 @@
 <?php
 
+$title = $email = $ingredients = '';
+
+$errors = array('email' => '', 'title' => '', 'ingredients' => '');
 if (isset($_POST['submit'])) {
 
     if (empty($_POST['email'])) {
-        echo 'An email is required <br/>';
+        $errors['email'] = 'An email is required <br/>';
     } else {
         $email = $_POST['email'];
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            echo 'Email must be valid';
+            $errors['email'] = 'Email must be valid';
         }
     }
 
     if (empty($_POST['title'])) {
-        echo 'A title is required <br/>';
+        $errors['title'] = 'A title is required <br/>';
     } else {
         $title = $_POST['title'];
         if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
-            echo 'Title must be letters and spaces only';
+            $errors['title'] = 'Title must be letters and spaces only';
         }
     }
 
     if (empty($_POST['ingredients'])) {
-        echo 'At least one ingredient is required <br/>';
+        $errors['ingredients'] = 'At least one ingredient is required <br/>';
     } else {
         $ingredients = $_POST['ingredients'];
         if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
-            echo 'Ingredients must be sperated by comma';
+            $errors['ingredients'] = 'Ingredients must be sperated by comma';
         }
     }
 }
@@ -42,11 +45,14 @@ if (isset($_POST['submit'])) {
     <h4 class="center">Add a pizza</h4>
     <form action="add.php" class="white" method="POST">
         <label>Your Email: </label>
-        <input type="text" name="email">
+        <div class="red-text"><?php echo $errors['email']; ?></div>
+        <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
         <label>Pizza Title: </label>
-        <input type="text" name="title">
+        <div class="red-text"><?php echo $errors['title']; ?></div>
+        <input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
         <label>Ingredients (comma seperated):</label>
-        <input type="text" name="ingredients">
+        <input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
+        <div class="red-text"><?php echo $errors['ingredients']; ?></div>
 
         <div class="center">
             <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
