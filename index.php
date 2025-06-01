c385588b157727e083dd6d499b52e2f28809af8d
Ravindu Samaraweera <155237294+rasandu17@users.noreply.github.com>
2025-06-01 17:39:00 +0530

    Connect with MySQL

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
 
-$conn = mysqli_connect("localhost","rukiee","pizza123","pizza_shop");
+$conn = mysqli_connect("localhost", "rukiee", "pizza123", "pizza_shop");
 
 if (!$conn) {
-    echo 'Connection error' .mysqli_connect_error();
+    echo 'Connection error' . mysqli_connect_error();
 }
 
-?>
+$sql = 'SELECT title,ingredients,id FROM pizzas';
+$result = mysqli_query($conn, $sql);
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+mysqli_free_result($result);
+mysqli_close($conn);
+
+print_r($pizzas)
+
+    ?>
 
 
 
@@ -14,8 +23,8 @@ if (!$conn) {
 <!DOCTYPE html>
 <html lang="en">
 
-<?php include('templates/header.php')?>
-<?php include('templates/footer.php')?>
-    
+<?php include('templates/header.php') ?>
+<?php include('templates/footer.php') ?>
+
 
 </html>
