cd8eb3ebafeb2722dd47a0b4e45a195209537671
Ravindu Samaraweera <155237294+rasandu17@users.noreply.github.com>
2025-06-01 18:09:03 +0530

    Create a template for display pizza

--- index.php
+++ index.php
@@ -6,16 +6,14 @@ if (!$conn) {
     echo 'Connection error' . mysqli_connect_error();
 }
 
-$sql = 'SELECT title,ingredients,id FROM pizzas';
+$sql = 'SELECT title,ingredients,id FROM pizzas ORDER BY created_at';
 $result = mysqli_query($conn, $sql);
 $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 mysqli_free_result($result);
 mysqli_close($conn);
 
-print_r($pizzas)
-
-    ?>
+?>
 
 
 
@@ -24,6 +22,26 @@ print_r($pizzas)
 <html lang="en">
 
 <?php include('templates/header.php') ?>
+<h4 class="center">Pizzas!!!</h4>
+<div class="container">
+    <div class="row">
+        <?php foreach($pizzas as $pizza){?>
+
+            <div class="col s6 md3">
+                <div class="card z-depth-0">
+                    <div class="card-content center">
+                        <h6><?php echo htmlspecialchars($pizza['title'])?></h6>
+                        <div><?php echo htmlspecialchars($pizza['ingredients'])?></div>
+                    </div>
+                    <div class="card-action right-align">
+                        <a class="brand-text" href="#">more info</a>
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
+    </div>
+</div>
+
 <?php include('templates/footer.php') ?>
 
 
