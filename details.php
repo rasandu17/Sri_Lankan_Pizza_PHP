d5394a05f562d9898cbbbd65fa99e5322b5d903d
Ravindu Samaraweera <155237294+rasandu17@users.noreply.github.com>
2025-06-05 16:10:45 +0530

    Refactor delete functionality in pizza details page for improved readability

--- details.php
+++ details.php
@@ -1,6 +1,23 @@
 <?php
 
+
 include('config/db_connect.php');
+
+$pizza = null;
+
+if(isset($_POST['delete'])){
+    $id_to_delete = mysqli_real_escape_string($conn,$_POST['id_to_delete']);
+
+    $sql = "DELETE FROM pizzas WHERE id=$id_to_delete";
+
+    if(mysqli_query($conn,$sql)){
+        header("Location:index.php");
+    }else{  
+        echo "Query error" . mysqli_error($conn);
+    }
+}
+
+
 
 if(isset($_GET['id'])){
 
@@ -30,6 +47,15 @@ if(isset($_GET['id'])){
     <p><?php echo date($pizza['created_at'])?></p>
     <h5>Ingredients:</h5>
     <p><?php echo htmlspecialchars($pizza['ingredients'])?></p>
+
+
+    <form action="details.php" method="POST"> 
+        <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id']?>">
+        <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+
+
+
+    </form>
 <?php else: ?>
     <h5>No such pizza exists!</h5>
 <?php endif?>
